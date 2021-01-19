c821b9bdeca74c6efea4a7fc930ae219f61823cb
LDFranck <43423861+LDFranck@users.noreply.github.com>
2021-01-19 08:59:37 -0300

    Update Codigo.hs

--- Atividade 1/Parte D/Codigo.hs
+++ Atividade 1/Parte D/Codigo.hs
@@ -3,14 +3,12 @@
 
 module Codigo where
     
-data ParteD a = Opcao1 a | Opcao2 a | Opcao3 a deriving Show
+ida :: (Maybe Bool, a) -> Either (Either a a) a
+ida (Just False, a) = Left (Left a)
+ida (Just True, a) = Left (Right a)
+ida (Nothing, a) = Right a
 
-ida :: (Maybe Bool, a) -> ParteD a
-ida (Just False, x) = Opcao1 x
-ida (Just True, x) = Opcao2 x
-ida (Nothing, x) = Opcao3 x
-
-volta :: ParteD a -> (Maybe Bool, a)
-volta (Opcao1 x) = (Just False, x)
-volta (Opcao2 x) = (Just True, x)
-volta (Opcao3 x) = (Nothing, x)
+volta :: Either (Either a a) a -> (Maybe Bool, a)
+volta (Left (Left a)) = (Just False, a)
+volta (Left (Right a)) = (Just True, a)
+volta (Right a) = (Nothing, a)
